d090cee106a6322925ecc6c23030743a91d4bee8
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-07-24 21:41:11 +0200

    fixed error

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ import * as components from './components/index.js';
 import { signature } from './utils/signature.js';
 import { manageCookies } from './utils/manageCookies.js';
 
+// GLOBAL VARIABLES
+const userLang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
+
 // CALLING FUNCTIONS
 document.addEventListener("DOMContentLoaded", async () => {
     activeJS('js-disabled', 'js-enabled');
 
-    const userLang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
-    initI18n(UserLang);
+    initI18n(userLang);
     
     components.themeDark('#theme-dark-btn');
     await components.initCarousel('.carousel-container', '.carousel-imgs', '.carousel-advance', '.carousel-back');
