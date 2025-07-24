9aa0e35b2c230403fd2cb4951227802365924393
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-07-24 21:25:58 +0200

    3

--- js/utils/activeJS.js
+++ js/utils/activeJS.js
@@ -1,4 +1,4 @@
-// REPLACE oldClass WITH newClass IN ELEMENTS
+// REPLACE .js-disabled WITH .js-enabled
 export function activeJS(oldClass = 'js-disabled', newClass = 'js-enabled') {
     document.querySelectorAll(`.${oldClass}`).forEach(el => {
         el.classList.remove(oldClass);
