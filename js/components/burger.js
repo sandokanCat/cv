010fae42a9edd476d2a42667d81747ab13a79dec
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-07-25 18:32:20 +0200

    debugging

--- js/components/burger.js
+++ js/components/burger.js
@@ -1,6 +1,6 @@
 export function openMenu() {
     const burgerBtn = document.getElementById("burger-btn");
-    const burguerLinks = document.getElementById("github-icon") && document.getElementById("vercel-icon");
+    const burgerIcons = document.getElementById("github-icon") && document.getElementById("vercel-icon");
 
     burgerBtn.addEventListener("click", () => {
         const isExpanded = burgerBtn.getAttribute("aria-expanded") === "true";
@@ -8,7 +8,7 @@ export function openMenu() {
 
         burgerBtn.setAttribute("aria-expanded", newState);
         burgerBtn.setAttribute("aria-label", newState ? "Cerrar menú" : "Abrir menú");
-        burguerLinks.classList.toggle("active");
+        burgerIcons.classList.toggle("active");
 
         console.log(
             "%c🍔 Menú desplegable hecho con CSS puro y amor. ¡No jQuery aquí!",
