558cd086f5e7ccfd6bc168e5cff1a785512d20ec
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-07-29 18:02:55 +0200

    fixing typo.error

--- js/components/index.js
+++ js/components/index.js
@@ -6,4 +6,4 @@ export { openModal } from './modal/modal.js';
 // I18N HELPERS
 export { reloadCarousel } from "./carousel.js";
 export { reloadRandomMsg } from "./randomPhrases.js";
-export { reloadProvisionalAlert } from "./provisionalAlerts.js";
+export { reloadProvisionalAlert } from "./provisionalAlert.js";
