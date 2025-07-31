2564ae3d630ec99e87d7e01d33ac910572e23c8f
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-07-31 08:21:50 +0200

    try dinamic aria-label

--- js/components/burger.js
+++ js/components/burger.js
@@ -7,31 +7,18 @@ const githubIcon = document.getElementById("github-icon");
 const vercelIcon = document.getElementById("vercel-icon");
 
 // OPEN HAMBURGER MENU
-export function openMenu(locale = getLocale()) {
-    const i18nData = getI18nData();
-    const labels = i18nData.burgerBtn?.["aria-label"];
+export function openMenu(locale) {
+    const { burgerBtn: burgerLabels } = getI18nData(locale);
+    const labels = burgerLabels?.["aria-label"];
 
     burgerBtn.addEventListener("click", () => {
         const isExpanded = burgerBtn.getAttribute("aria-expanded") === "true";
         const newState = !isExpanded;
-        
-        // GET I18N LABEL
-        burgerBtn.setAttribute("aria-label", labels?.[newState ? "close" : "open"] || "Menu");
 
         // SET BTN ARIA STATES
         burgerBtn.setAttribute("aria-expanded", newState.toString());
-        burgerBtn.setAttribute("aria-pressed", newState.toString()); burgerBtn.setAttribute("aria-label", labels?.[newState ? "close" : "open"] || "Menu");
-        burgerBtn.setAttribute("aria-label", label || "Open menu"); // en-GB FALLBACK
-
-        // switch (locale) { // ARIA-LABEL WITH I18N
-        //     case "es-ES":
-        //         burgerBtn.setAttribute("aria-label", newState ? "Cerrar menú" : "Abrir menú");
-        //         break;
-        //     case "ca-ES":
-        //         burgerBtn.setAttribute("aria-label", newState ? "Tancar menú" : "Obrir menú");
-        //         break;
-        //     default: burgerBtn.setAttribute("aria-label", newState ? "Close menu" : "Open menu");
-        // }
+        burgerBtn.setAttribute("aria-pressed", newState.toString());
+        burgerBtn.setAttribute("aria-label", labels?.[newState ? "close" : "open"] || "Open menu"); // FALLBACK
 
         // TOGGLE ICONS
         githubIcon.classList.toggle("active");
