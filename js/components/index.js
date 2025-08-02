494850cb12aa2b3abbbc83ade8200658758ef4f8
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-08-02 15:04:17 +0200

    comment openModal for debug

--- js/components/index.js
+++ js/components/index.js
@@ -2,7 +2,7 @@
 export { themeDark } from './themeDark.js';
 export { initCarousel } from "./carousel.js";
 export { openBurger, updateBurgerData } from './burger.js';
-export { openModal } from './modal/modal.js';
+// export { openModal } from './modal/modal.js';
 
 // I18N HELPERS
 export { updateCarouselAlts } from "./carousel.js";
