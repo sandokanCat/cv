3acfc6817b3f3eeab5e35ec3b8d2d43541dde3f1
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-08-08 09:02:17 +0200

    f

--- js/utils/index.js
+++ js/utils/index.js
@@ -5,6 +5,7 @@ export {
     fallbackLocale,
     getI18nData,
     initI18n,
+    setLocaleStorage,
     reloadDynamicContent } from './i18n.js';
 export { initToggler } from './initToggler.js';
 export { signature } from './signature.js';
