adc6aaebc5146f42f8490c62898d45cea16570ce
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-11-12 22:08:48 +0100

    Refactor exports in index.js
    Removed unused exports and added manageCookies export.

--- js/utils/index.js
+++ js/utils/index.js
@@ -1,5 +1,4 @@
 // EXPORTS
-export { default as logger } from '../../app/open-utils/public/js/logger.js';
 export { replaceClass } from './replaceClass.js';
 export {
     getLocale,
@@ -8,8 +7,6 @@ export {
     initI18n,
     setLocaleStorage,
     reloadDynamicContent } from './i18n.js';
-export { validateJSON } from '../../app/open-utils/public/js/validateJSON.js';
-export { validateCarousel } from '../../app/open-utils/public/js/validateCarousel.js';
 export { initToggler } from './initToggler.js';
 export { signature } from './signature.js';
 export { manageCookies } from './manageCookies.js';
