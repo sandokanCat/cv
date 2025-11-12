0783d5ab665a414937072bd36a58f36488b64acd
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2025-11-12 22:27:43 +0100

    Refactor randomPhrases.js with improved imports

--- js/components/randomPhrases.js
+++ js/components/randomPhrases.js
@@ -1,6 +1,7 @@
 // IMPORTS
+import { logger } from "https://open-utils-dev-sandokan-cat.vercel.app/js/logger.js";
 import { validateJSON } from "https://open-utils-dev-sandokan-cat.vercel.app/js/validateJSON.js"
-import { logger, getLocale } from "../utils/index.js"; // USE GLOBAL i18n LOCALE DETECTION
+import { getLocale } from "../utils/index.js"; // USE GLOBAL i18n LOCALE DETECTION
 
 // GLOBAL VARIABLES
 const json = "./js/data/phrases.json"; // SOURCE JSON FILE
@@ -107,3 +108,4 @@ async function showRandomMsg(locale = getLocale()) {
     }
 
 }
+
