07ec042c892834aae8e21b0cd4866501aaa6c6dc
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2026-02-10 08:37:25 +0100

    add .ico fallback

--- partials/head.php
+++ partials/head.php
@@ -63,8 +63,8 @@ if (!defined('ENTRY_POINT')) {
     <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/tech-cat-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/tech-cat-32x32.png">
     <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/tech-cat-180x180.png">
-    <!-- <link rel="icon" type="image/svg+xml" sizes="any" href="img/favicon/tech-cat.svg">
-    <link rel="icon" type="image/x-icon" sizes="any" href="img/favicon/tech-cat.ico"> -->
+    <!-- <link rel="icon" type="image/svg+xml" sizes="any" href="img/favicon/tech-cat.svg"> -->
+    <link rel="icon" type="image/x-icon" sizes="any" href="img/favicon/tech-cat.ico">
     <link rel="manifest" href="site.webmanifest">
 
     <!-- PRECONNECT & DNS-PREFETCH -->
