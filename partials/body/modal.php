736d5bcab2446ebe824e27de7b8d8c32ba14d7a6
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2026-02-21 14:57:03 +0100

    add sandbox attr

--- partials/body/modal.php
+++ partials/body/modal.php
@@ -11,14 +11,11 @@ if (!defined('ENTRY_POINT')) {
 <div id="modal-container" role="dialog" aria-hidden="true" tabindex="-1">
     <div id="modal-content">
         <div id="modal-iframe-wrapper">
-            <iframe data-i18n-attr="aria-label:modalIframe" id="modal-iframe" src="about:blank"
-                aria-label="<?= $L('modalIframe'); ?>"></iframe>
+            <iframe data-i18n-attr="aria-label:modalIframe" id="modal-iframe" src="about:blank" sandbox="allow-scripts allow-forms allow-same-origin allow-popups" aria-label="<?= $L('modalIframe'); ?>"></iframe>
         </div>
         <div id="modal-img-wrapper"></div>
-        <button data-i18n-attr="aria-label:modalClose" id="modal-close" class="icons-color-1"
-            aria-label="<?= $L('modalClose'); ?>" type="button">
-            <svg class="icons-snippet active" aria-hidden="true" width="50" height="50"
-                preserveAspectRatio="xMinYMin meet">
+        <button data-i18n-attr="aria-label:modalClose" id="modal-close" class="icons-color-1" aria-label="<?= $L('modalClose'); ?>" type="button">
+            <svg class="icons-snippet active" aria-hidden="true" width="50" height="50" preserveAspectRatio="xMinYMin meet">
                 <use href="img/sprite.svg#close" xlink:href="img/sprite.svg#close"></use>
             </svg>
         </button>
